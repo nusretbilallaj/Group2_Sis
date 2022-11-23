5dad4714f973e52ca9ed2aae0658a900b69422d5
Nusret Bilallaj <netktit@gmail.com>
2022-11-23 18:41:45 +0100

    Add project files.

---
+++ Group2_Sis/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Group2_Sis.Data;
+using Group2_Sis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group2_Sis.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly Konteksti _konteksti;
+        public StudentController(Konteksti kon)
+        {
+            _konteksti = kon;
+        }
+        public IActionResult Listo()
+        {
+           List<Studenti> studentet= _konteksti.Studentet.ToList();
+            return View(studentet);
+        }
+    }
+}
---
+++ Group2_Sis/Data/Konteksti.cs
@@ -0,0 +1,18 @@
+﻿using Group2_Sis.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Group2_Sis.Data
+{
+    public class Konteksti:DbContext
+    {
+        public Konteksti(DbContextOptions<Konteksti> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Studenti> Studentet { get; set; }
+        public DbSet<Komuna> Komunat { get; set; }
+
+    }
+}
